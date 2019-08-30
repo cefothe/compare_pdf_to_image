2636abdf3454fc68b95e7ee148bcf3c357e03c8f
cefothe <cefothe@gmail.com>
2019-08-30 13:15:28 +0300

    create ui mainwindow xml file from Qt Creator

--- views.py
+++ views.py
@@ -1,9 +1,15 @@
 import numpy as np
 from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QVBoxLayout, QApplication, QSlider
+from PyQt5 import uic
 
 class StartWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi("./ui/mainwindow.ui", self)
+        self.save_pdf.clicked.connect(self.btnClicked)
+
+    def btnClicked(self):
+        print("Text")
 
 
 if __name__ == '__main__':
